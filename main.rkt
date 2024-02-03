74d235e0799f3cc649a2647d5318cb58b998099b
Tyler Baxter <tybaxter@calpoly.edu>
2024-02-03 11:39:28 -0800

    parse-fundef

--- main.rkt
+++ main.rkt
@@ -3,14 +3,19 @@
 
 
 ;Arith - language definition
+;expressions
 (define-type ExprC (U NumC BinopC IdC Ifleq0C))
 (struct NumC    ([n : Real]) #:transparent)
 (struct BinopC  ([operation : Symbol] [l : ExprC] [r : ExprC]) #:transparent)
 (struct AppC    ([funid : Symbol] [param : ExprC]) #:transparent)
 (struct IdC     ([id : Symbol]) #:transparent)
 (struct Ifleq0C ([c : ExprC] [y : ExprC] [n : ExprC]) #:transparent)
+;functions
+(struct FundefC ([kw : Symbol] [funid : IdC] [param : IdC] [colon : Symbol] [exp : ExprC]) #:transparent)
 
-;(struct FundefC)
+
+
+
 
 ;parse
  ;in: s-expression code
@@ -24,8 +29,7 @@
     [(list '* l r) (BinopC '* (parse l) (parse r))]
     [(list '/ l r) (BinopC '/ (parse l) (parse r))]
     [(list 'ifleq0? f s r) (Ifleq0C (parse f) (parse s) (parse r))]
-    [other (error 'OAZO3-parse "syntax error in ~e" other)]))
-
+    [other (error 'parse "OAZO3 syntax error in ~e" other)]))
 ;tests
 (check-equal? (parse 1) (NumC 1))
 (check-equal? (parse 'y) (IdC 'y))
@@ -35,6 +39,21 @@
 (check-equal? (parse '{/ 1 2}) (BinopC '/ (NumC 1) (NumC 2)))
 (check-equal? (parse '{ifleq0? -1 5 3}) (Ifleq0C (NumC -1) (NumC 5) (NumC 3)))
 (check-exn #rx"syntax error" (lambda () (parse '(+ 2))))
+
+
+;parse-fundef
+ ;in: s-expression code
+ ;out: the parsed FundefC representation of code
+(define (parse-fundef [code : Sexp]) : FundefC
+  (match code
+    [(list 'func (? symbol? n) (? symbol? p) ': e) (FundefC 'func (cast (parse n) IdC) (cast (parse p) IdC) ': (parse e))]
+    [other (error 'parse-fundef "OAZO3 syntax error in ~e" other)]))
+;tests
+(check-equal? (parse-fundef '{func name param : {+ 1 2}})
+              (FundefC 'func (IdC 'name) (IdC 'param) ': (BinopC '+ (NumC 1) (NumC 2))))
+(check-exn #rx"syntax error" (lambda () (parse-fundef '{notafunc name param : {+ 1 2}})))
+(check-exn #rx"syntax error" (lambda () (parse-fundef '(+ 2))))
+
 
 
 
@@ -51,7 +70,6 @@
     [(BinopC '/ l r)  (/ (interp l) (interp r))]
     [(Ifleq0C c y n) (cond [(<= (interp c) 0) (interp y)]
                            [else (interp n)])]))
-
 ;tests
 (check-equal? (interp (BinopC '+ (NumC 3) (NumC 4))) 7)
 (check-equal? (interp (BinopC '* (NumC 3) (NumC 4))) 12)
@@ -59,13 +77,14 @@
 (check-equal? (interp (BinopC '/ (NumC 3) (NumC 4))) 3/4)
 
 
- 
+
+
+
 ;top-interp
  ;in: s-expression s
  ;out: the interpretation of the parse of s
 (define (top-interp [s : Sexp]) : Real
   (interp (parse s)))
-
 ;tests
 (check-equal? (top-interp '(+ 2 3)) 5)
 (check-equal? (top-interp '(- 3 3)) 0)
