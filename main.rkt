a4608a344697ba4b3d9d35da5433b6456648fb93
Tyler Baxter <tybaxter@calpoly.edu>
2024-02-03 11:08:33 -0800

    captial structs

--- main.rkt
+++ main.rkt
@@ -3,67 +3,66 @@
 
 
 ;Arith - language definition
-(define-type ExprC (U numC binopC idC ifleq0C))
-(struct numC    ([n : Real]) #:transparent)
-(struct binopC  ([operation : Symbol] [l : ExprC] [r : ExprC]) #:transparent)
-(struct appC    ([funid : Symbol] [param : ExprC]) #:transparent)
-(struct idC     ([id : Symbol]) #:transparent)
-(struct ifleq0C ([c : ExprC] [y : ExprC] [n : ExprC]) #:transparent)
+(define-type ExprC (U NumC BinopC IdC Ifleq0C))
+(struct NumC    ([n : Real]) #:transparent)
+(struct BinopC  ([operation : Symbol] [l : ExprC] [r : ExprC]) #:transparent)
+(struct AppC    ([funid : Symbol] [param : ExprC]) #:transparent)
+(struct IdC     ([id : Symbol]) #:transparent)
+(struct Ifleq0C ([c : ExprC] [y : ExprC] [n : ExprC]) #:transparent)
 
-
-
+;(struct FundefC)
 
 ;parse
- ;accepts an s-expression code
- ;returns the parsed ExprC representation of code
+ ;in: s-expression code
+ ;out: the parsed ExprC representation of code
 (define (parse [code : Sexp]) : ExprC
   (match code
-    [(? real? n)   (numC n)]
-    [(? symbol? s) (idC s)]
-    [(list '+ l r) (binopC '+ (parse l) (parse r))]
-    [(list '- l r) (binopC '- (parse l) (parse r))]
-    [(list '* l r) (binopC '* (parse l) (parse r))]
-    [(list '/ l r) (binopC '/ (parse l) (parse r))]
-    [(list 'ifleq0? f s r) (ifleq0C (parse f) (parse s) (parse r))]
+    [(? real? n)   (NumC n)]
+    [(? symbol? s) (IdC s)]
+    [(list '+ l r) (BinopC '+ (parse l) (parse r))]
+    [(list '- l r) (BinopC '- (parse l) (parse r))]
+    [(list '* l r) (BinopC '* (parse l) (parse r))]
+    [(list '/ l r) (BinopC '/ (parse l) (parse r))]
+    [(list 'ifleq0? f s r) (Ifleq0C (parse f) (parse s) (parse r))]
     [other (error 'OAZO3-parse "syntax error in ~e" other)]))
 
 ;tests
-(check-equal? (parse 1) (numC 1))
-(check-equal? (parse 'y) (idC 'y))
-(check-equal? (parse '{+ 1 2}) (binopC '+ (numC 1) (numC 2)))
-(check-equal? (parse '{- 1 2}) (binopC '- (numC 1) (numC 2)))
-(check-equal? (parse '{* 1 2}) (binopC '* (numC 1) (numC 2)))
-(check-equal? (parse '{/ 1 2}) (binopC '/ (numC 1) (numC 2)))
-(check-equal? (parse '{ifleq0? -1 5 3}) (ifleq0C (numC -1) (numC 5) (numC 3)))
+(check-equal? (parse 1) (NumC 1))
+(check-equal? (parse 'y) (IdC 'y))
+(check-equal? (parse '{+ 1 2}) (BinopC '+ (NumC 1) (NumC 2)))
+(check-equal? (parse '{- 1 2}) (BinopC '- (NumC 1) (NumC 2)))
+(check-equal? (parse '{* 1 2}) (BinopC '* (NumC 1) (NumC 2)))
+(check-equal? (parse '{/ 1 2}) (BinopC '/ (NumC 1) (NumC 2)))
+(check-equal? (parse '{ifleq0? -1 5 3}) (Ifleq0C (NumC -1) (NumC 5) (NumC 3)))
 (check-exn #rx"syntax error" (lambda () (parse '(+ 2))))
 
 
 
 ;interp
- ;accepts an ExprC a
- ;returns the evaluation of a
+ ;in: ExprC a
+ ;out: evaluation of a
 (define (interp [a : ExprC]) : Real
   (match a
-    [(numC n) n]
-    ;[(idC s) s]
-    [(binopC '+ l r) (+ (interp l) (interp r))]
-    [(binopC '- l r)  (- (interp l) (interp r))]
-    [(binopC '* l r) (* (interp l) (interp r))]
-    [(binopC '/ l r)  (/ (interp l) (interp r))]
-    [(ifleq0C c y n) (cond [(<= (interp c) 0) (interp y)]
+    [(NumC n) n]
+    ;[(IdC s) s]
+    [(BinopC '+ l r) (+ (interp l) (interp r))]
+    [(BinopC '- l r)  (- (interp l) (interp r))]
+    [(BinopC '* l r) (* (interp l) (interp r))]
+    [(BinopC '/ l r)  (/ (interp l) (interp r))]
+    [(Ifleq0C c y n) (cond [(<= (interp c) 0) (interp y)]
                            [else (interp n)])]))
 
 ;tests
-(check-equal? (interp (binopC '+ (numC 3) (numC 4))) 7)
-(check-equal? (interp (binopC '* (numC 3) (numC 4))) 12)
-(check-equal? (interp (binopC '- (numC 3) (numC 4))) -1)
-(check-equal? (interp (binopC '/ (numC 3) (numC 4))) 3/4)
+(check-equal? (interp (BinopC '+ (NumC 3) (NumC 4))) 7)
+(check-equal? (interp (BinopC '* (NumC 3) (NumC 4))) 12)
+(check-equal? (interp (BinopC '- (NumC 3) (NumC 4))) -1)
+(check-equal? (interp (BinopC '/ (NumC 3) (NumC 4))) 3/4)
 
 
  
 ;top-interp
- ;accepts s-expression s
- ;returns the interpretation of the parse of s
+ ;in: s-expression s
+ ;out: the interpretation of the parse of s
 (define (top-interp [s : Sexp]) : Real
   (interp (parse s)))
 
@@ -76,6 +75,4 @@
 (check-equal? (top-interp '(ifleq0? -1 (+ 2 3) (- 2 3))) 5)
 (check-exn #rx"syntax error" (lambda () (top-interp '(+ 2))))
 (check-exn #rx"syntax error" (lambda () (top-interp '())))
-
-
 
