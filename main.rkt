9852b1cc717906ea13741e49a751ce56ced14184
Tyler Baxter <tybaxter@calpoly.edu>
2024-02-03 10:49:59 -0800

    start

--- main.rkt
+++ main.rkt
@@ -2,84 +2,67 @@
 (require typed/rackunit)
 
 
-;Arith language declaration
-
 ;Arith - language definition
-(define-type ArithC (U numC plusC multC squareC))
-(struct numC ([n : Real]) #:transparent)
-(struct plusC ([l : ArithC] [r : ArithC]) #:transparent)
-(struct multC ([l : ArithC] [r : ArithC]) #:transparent)
-(struct squareC ([n : ArithC]) #:transparent)
-
-
-
-
-
-;interp
- ;accepts an ArithC a
- ;returns the evaluation of a
-(define (interp [a : ArithC]) : Real
-  (match a
-    [(numC n) n]
-    [(plusC l r) (+ (interp l) (interp r))]
-    [(multC l r) (* (interp l) (interp r))]
-    [(squareC n) (expt (interp n) 2)]))
-
-;tests
-(check-equal? (interp (plusC (numC 3) (numC 4))) 7)
-(check-equal? (interp (multC (numC 3) (numC 4))) 12)
-(check-equal? (interp (multC (plusC (numC 4) (numC 4)) (numC 4))) 32)
-(check-equal? (interp (squareC (numC 4))) 16)
-
-
-
-
-
-;num-adds
- ;accepts an ArithC a
- ;prints the tree upon entry
- ;returns the number of additions in a
-(define (num-adds [a : ArithC]) : Real
-  (match a
-    [(numC n)    (printf "(numC ~e)\n" n)
-                 0]
-    [(plusC l r) (printf "(plusC\n    ~e\n    ~e)\n" l r)
-                 (+ 1 (num-adds l) (num-adds r))]
-    [(multC l r) (printf "(multC\n    ~e\n    ~e)\n" l r)
-                 (+ (num-adds l) (num-adds r))]
-    [(squareC n) (printf "(squareC\n    ~e)" n)
-                 (num-adds n)]))
-
-(check-equal? (num-adds (multC (plusC (numC 4) (numC 4)) (numC 4))) 1)
-(check-equal? (num-adds (plusC (plusC (numC 3) (numC 4)) (plusC (numC 3) (numC 4)))) 3)
-(check-equal? (num-adds (plusC (plusC (squareC (numC 3)) (numC 4)) (plusC (numC 3) (numC 4)))) 3)
-(check-equal? (num-adds (multC (numC 3) (numC 4))) 0)
-
-
+(define-type ExprC (U numC plusC subC multC divC idC ifleq0C))
+(struct numC  ([n : Real]) #:transparent)
+(struct plusC ([l : ExprC] [r : ExprC]) #:transparent)
+(struct subC  ([l : ExprC] [r : ExprC]) #:transparent)
+(struct multC ([l : ExprC] [r : ExprC]) #:transparent)
+(struct divC  ([l : ExprC] [r : ExprC]) #:transparent)
+(struct appC  ([funid : Symbol] [param : ExprC]) #:transparent)
+(struct idC   ([id : Symbol]) #:transparent)
+(struct ifleq0C ([c : ExprC] [y : ExprC] [n : ExprC]) #:transparent)
+(struct binopC ([operation : Symbol] [l : ExprC] [r : ExprC]) #:transparent)
 
 
 
 ;parse
  ;accepts an s-expression code
- ;returns the parsed ArithC representation of code
-(define (parse [code : Sexp]) : ArithC
+ ;returns the parsed ExprC representation of code
+(define (parse [code : Sexp]) : ExprC
   (match code
     [(? real? n)   (numC n)]
+    [(? symbol? s) (idC s)]
     [(list '+ l r) (plusC (parse l) (parse r))]
+    [(list '- l r) (subC (parse l) (parse r))]
     [(list '* l r) (multC (parse l) (parse r))]
-    [(list '^2 n)  (squareC (parse n))]
-    [other (error 'parse "syntax error in ~e" other)]))
+    [(list '/ l r) (divC (parse l) (parse r))]
+    [(list 'ifleq0? f s r) (ifleq0C (parse f) (parse s) (parse r))]
+    [other (error 'OAZO3-parse "syntax error in ~e" other)]))
 
 ;tests
+(check-equal? (parse 1) (numC 1))
+(check-equal? (parse 'y) (idC 'y))
 (check-equal? (parse '{+ 1 2}) (plusC (numC 1) (numC 2)))
+(check-equal? (parse '{- 1 2}) (subC (numC 1) (numC 2)))
 (check-equal? (parse '{* 1 2}) (multC (numC 1) (numC 2)))
-(check-equal? (parse '{^2 2}) (squareC (numC 2)))
+(check-equal? (parse '{/ 1 2}) (divC (numC 1) (numC 2)))
+(check-equal? (parse '{ifleq0? -1 5 3}) (ifleq0C (numC -1) (numC 5) (numC 3)))
 (check-exn #rx"syntax error" (lambda () (parse '(+ 2))))
 
 
 
+;interp
+ ;accepts an ExprC a
+ ;returns the evaluation of a
+(define (interp [a : ExprC]) : Real
+  (match a
+    [(numC n) n]
+    ;[(idC s) s]
+    [(plusC l r) (+ (interp l) (interp r))]
+    [(subC l r)  (- (interp l) (interp r))]
+    [(multC l r) (* (interp l) (interp r))]
+    [(divC l r)  (/ (interp l) (interp r))]
+    [(ifleq0C c y n) (cond [(<= (interp c) 0) (interp y)]
+                           [else (interp n)])]))
+
+;tests
+(check-equal? (interp (plusC (numC 3) (numC 4))) 7)
+(check-equal? (interp (multC (numC 3) (numC 4))) 12)
+(check-equal? (interp (multC (plusC (numC 4) (numC 4)) (numC 4))) 32)
 
 
+ 
 ;top-interp
  ;accepts s-expression s
  ;returns the interpretation of the parse of s
@@ -87,10 +70,12 @@
   (interp (parse s)))
 
 ;tests
-(check-equal? (top-interp '(* 2 3)) 6)
 (check-equal? (top-interp '(+ 2 3)) 5)
-(check-equal? (top-interp '(^2 2)) 4)
-(check-equal? (top-interp '(+ (* (^2 5) 4) 100)) 200)
+(check-equal? (top-interp '(- 3 3)) 0)
+(check-equal? (top-interp '(* 2 3)) 6)
+(check-equal? (top-interp '(/ 4 2)) 2)
+(check-equal? (top-interp '(ifleq0? 1 (+ 2 3) (- 2 3))) -1)
+(check-equal? (top-interp '(ifleq0? -1 (+ 2 3) (- 2 3))) 5)
 (check-exn #rx"syntax error" (lambda () (top-interp '(+ 2))))
 (check-exn #rx"syntax error" (lambda () (top-interp '())))
 
