c931659663b9fdf2d08a647e159cc691c082a6e6
Tyler Baxter <tybaxter@calpoly.edu>
2024-02-04 16:05:48 -0800

    fixed parse

--- main.rkt
+++ main.rkt
@@ -4,14 +4,14 @@
 
 ;Arith - language definition
 ;expressions
-(define-type ExprC (U NumC BinopC IdC Ifleq0C))
+(define-type ExprC (U NumC BinopC IdC Ifleq0C AppC))
 (struct NumC    ([n : Real]) #:transparent)                                   ;numbers
 (struct BinopC  ([operation : Symbol] [l : ExprC] [r : ExprC]) #:transparent) ;+-*/
 (struct AppC    ([funid : Symbol] [param : ExprC]) #:transparent)             ;function call
 (struct IdC     ([id : Symbol]) #:transparent)                                ;variable
 (struct Ifleq0C ([c : ExprC] [y : ExprC] [n : ExprC]) #:transparent)          ;simple conditional
 ;functions
-(struct FundefC ([kw : Symbol] [funid : IdC] [param : IdC] [colon : Symbol] [exp : ExprC]) #:transparent)
+(struct FundefC ([kw : Symbol] (Ids : (Listof IdC)) [colon : Symbol] [exp : ExprC]) #:transparent)
 
 
 
@@ -19,8 +19,13 @@
 
 ;function definitions for a runable program
 (define funs '{
-              {func adder param : {+ 1 2}}
-              {func suber x : {- 1 x}}
+               {func {adder param} : {+ 1 2}}
+               {func {suber x} : {- 1 x}}
+              })
+
+(define test '{
+               {func {f x} : {+ x 14}}
+               {func {main init} : {f 2}}
               })
 
 
@@ -39,6 +44,7 @@
     [(list '* l r) (BinopC '* (parse l) (parse r))]
     [(list '/ l r) (BinopC '/ (parse l) (parse r))]
     [(list 'ifleq0? f s r) (Ifleq0C (parse f) (parse s) (parse r))]
+    [(list name param) (AppC (cast name Symbol) (parse param))]
     [other (error 'parse "OAZO3 syntax error in ~e" other)]))
 ;tests
 (check-equal? (parse 1) (NumC 1))
@@ -56,11 +62,11 @@
  ;out: the parsed FundefC representation of code
 (define (parse-fundef [code : Sexp]) : FundefC
   (match code
-    [(list 'func (? symbol? n) (? symbol? p) ': e) (FundefC 'func (cast (parse n) IdC) (cast (parse p) IdC) ': (parse e))]
+    [(list 'func (list (? symbol? n) (? symbol? p)) ': e) (FundefC 'func (list (cast (parse n) IdC) (cast (parse p) IdC)) ': (parse e))]
     [other (error 'parse-fundef "OAZO3 syntax error in ~e" other)]))
 ;tests
-(check-equal? (parse-fundef '{func name param : {+ 1 2}})
-              (FundefC 'func (IdC 'name) (IdC 'param) ': (BinopC '+ (NumC 1) (NumC 2))))
+(check-equal? (parse-fundef '{func (name param) : {+ 1 2}})
+              (FundefC 'func (list (IdC 'name) (IdC 'param)) ': (BinopC '+ (NumC 1) (NumC 2))))
 (check-exn #rx"syntax error" (lambda () (parse-fundef '{notafunc name param : {+ 1 2}})))
 (check-exn #rx"syntax error" (lambda () (parse-fundef '(+ 2))))
 
@@ -75,13 +81,47 @@
 ;tests
 (check-equal? (parse-prog funs)
               (list
-               (FundefC 'func (IdC 'adder) (IdC 'param) ': (BinopC '+ (NumC 1) (NumC 2)))
-               (FundefC 'func (IdC 'suber) (IdC 'x) ': (BinopC '- (NumC 1) (IdC 'x)))))
+               (FundefC 'func (list (IdC 'adder) (IdC 'param)) ': (BinopC '+ (NumC 1) (NumC 2)))
+               (FundefC 'func (list (IdC 'suber) (IdC 'x)) ': (BinopC '- (NumC 1) (IdC 'x)))))
 (check-exn #rx"syntax error" (lambda () (parse-prog '(+ 2))))
 
 
 
 
+
+;interp
+ ;in: ExprC exp, list of FundefC lst
+ ;out: evaliation of exp as a Real
+(define (interp [exp : ExprC] [lst : (Listof FundefC)]) : Real
+  (match exp
+    [(NumC n) n]
+    [(BinopC '+ l r) (+ (interp l lst) (interp r lst))]
+    [(BinopC '- l r)  (- (interp l lst) (interp r lst))]
+    [(BinopC '* l r) (* (interp l lst) (interp r lst))]
+    [(BinopC '/ l r)  (/ (interp l lst) (interp r lst))]
+    [(Ifleq0C c y n) (cond [(<= (interp c lst) 0) (interp y lst)]
+                           [else (interp n lst)])]
+    [(AppC funid param) 0]
+    [(IdC s) 0]))
+;tests
+
+
+
+;interp-fns
+ ;in: list of FundefC funcs
+ ;out: evaluation of funtions as a Real
+(define (interp-fns [funcs : (Listof FundefC)]) : Real
+  0)
+;tests
+;(check-equal? (interp-fns) 0)
+
+
+;top-interp
+
+
+
+
+#|
 
 ;interp
  ;in: ExprC a
@@ -121,3 +161,4 @@
 (check-exn #rx"syntax error" (lambda () (top-interp '(+ 2))))
 (check-exn #rx"syntax error" (lambda () (top-interp '())))
 
+|#
