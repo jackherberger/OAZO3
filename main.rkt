4a404c2e55fa7f47a1f7cff713a2762e582aa792
Tyler Baxter <tybaxter@calpoly.edu>
2024-02-05 17:47:46 -0800

    final final final

--- main.rkt
+++ main.rkt
@@ -13,13 +13,11 @@
 ;functions
 (struct FundefC ([name : Symbol] [arg : Symbol] [body : ExprC]) #:transparent)
 
-
 ;top-interp 
 ;in: list of oazo3 syntax functions fun-sexps
 ;out: the evaluation of main function in fun-sexps
 (define (top-interp [fun-sexps : Sexp]) : Real
   (interp-fns (parse-prog fun-sexps)))
-
 
 ;interp-fns
 ;in: list of FundefC funcs
@@ -28,7 +26,6 @@
   (define main-init (get-fundef 'main funs))
   (define main-zero (FundefC 'main (FundefC-arg main-init) (subst (NumC 0) 'init (FundefC-body main-init))))
   (interp (FundefC-body main-zero) funs))
-
 
 ;get-fundef
 ;in: a symbol n and list of FundefC fds
@@ -49,7 +46,8 @@
     [(BinopC '+ l r) (+ (interp l funs) (interp r funs))]
     [(BinopC '- l r)  (- (interp l funs) (interp r funs))]
     [(BinopC '* l r) (* (interp l funs) (interp r funs))]
-    [(BinopC '/ l r)  (/ (interp l funs) (interp r funs))]
+    [(BinopC '/ l r)  (cond [(not (eq? (interp r funs) 0)) (/ (interp l funs) (interp r funs))]
+                            [else (error 'interp "OAZO3 div by 0")])] 
     [(Ifleq0C c y n) (cond [(<= (interp c funs) 0) (interp y funs)]
                            [else (interp n funs)])]
     [(AppC f a) (define fd (get-fundef f funs))
@@ -57,11 +55,11 @@
                                (FundefC-arg fd)
                                (FundefC-body fd))
                         funs)]
-    [(IdC _) (error 'interp "OAZO3 shouldn't get here")]))
+    [(IdC _) (error 'interp "OAZO3 shouldn't get here")])) 
 
 ;subst
 ;in: ExprC what, Symbol for, and ExprC in
-;out: 
+;out: ExprC in with all for replaced by what
 (define (subst [what : ExprC] [for : Symbol] [in : ExprC]) : ExprC
   (match in
     [(NumC n) in]
@@ -76,8 +74,9 @@
     [(BinopC '- l r) (BinopC '- (subst what for l)
                              (subst what for r))]
     [(BinopC '/ l r) (BinopC '/ (subst what for l)
-                             (subst what for r))]))
-
+                             (subst what for r))]
+    [(Ifleq0C c y n) (Ifleq0C (subst what for c) (subst what for y)
+                              (subst what for n))]))
 
 ;parse-prog
 ;in: s-expression code
@@ -92,7 +91,10 @@
 ;out: the parsed FundefC representation of code
 (define (parse-fundef [code : Sexp]) : FundefC
   (match code
-    [(list 'func (list (? symbol? n) (? symbol? p)) ': e) (FundefC n p (parse e))]
+    [(list 'func (list (? symbol? n) (? symbol? p)) ': e)
+     (cond
+       [(and (is-allowed? n) (is-allowed? p)) (FundefC n p (parse e))]
+       [else (error 'parse "OAZO3 key word ~e videntified" n)])]
     [other (error 'parse-fundef "OAZO3 syntax error in ~e" other)]))
 
 ;parse
@@ -109,12 +111,14 @@
     [(list '/ l r) (BinopC '/ (parse l) (parse r))]
     [(list s l r) (error 'parse "OAZO3 ~e operation not supported" s)]
     [(list 'ifleq0? f s r) (Ifleq0C (parse f) (parse s) (parse r))]
-    [(list (? symbol? name) param) (AppC name (parse param))]
+    [(list (? symbol? name) param) (cond
+                                     [(is-allowed? name) (AppC name (parse param))]
+                                     [else (error 'parse "OAZO3 syntax error in ~e" name)])]
     [other (error 'parse "OAZO3 syntax error in ~e" other)]))
 
 ;is-allowed?
- ;takes in a symbol
- ;returns false if it is a keyword, otherwise true
+;takes in a symbol
+;returns false if it is a keyword, otherwise true
 (define (is-allowed? [s : Symbol]) : Boolean
   (match s
     ['+ #f]
@@ -125,7 +129,6 @@
     ['func #f]
     [': #f]
     [else #t]))
-
 
 
 
@@ -148,15 +151,12 @@
                       (FundefC 'adder 'y (BinopC '+ (IdC 'y) (NumC 2)))
                       (FundefC 'suber 'x (AppC 'adder (IdC 'x)))
                       (FundefC 'main 'param (AppC 'suber (NumC 10)))))
-
 (define testSub '{{func {adder y} : {- y 2}}
                   {func {suber x} : {adder x}}
                   {func {main param} : {suber 10}}})
-
 (define testMult '{{func {adder y} : {* y 2}}
                    {func {suber x} : {adder x}}
                    {func {main param} : {suber 10}}})
-
 (define testDiv '{{func {adder y} : {/ y 2}}
                   {func {suber x} : {adder x}}
                   {func {main param} : {suber 10}}})
@@ -168,7 +168,10 @@
 (check-equal? (top-interp testSub) 8)
 (check-equal? (top-interp testMult) 20)
 (check-equal? (top-interp testDiv) 5)
-
+(check-equal? (top-interp (quote ((func (main init) : (ifleq0? (* 3 1) 3 (+ 2 9)))))) 11)
+(check-equal? (top-interp (quote ((func (main init) : (+ (f 13) (f 0))) (func (f qq) : (ifleq0? qq qq (+ qq 1)))))) 14)
+(check-exn #rx"div" (lambda()
+                      (top-interp '((func (ignoreit x) : (+ 3 4)) (func (main init) : (ignoreit (/ 1 (+ 0 0)))))))) 
 
 ;interp-fns
 (check-equal? (interp-fns (parse-prog test0)) 3)
@@ -186,23 +189,16 @@
 (define tester (parse-prog'{{func {main init} : {- 1 2}}}))
 (define sub (BinopC '- (NumC 3) (NumC 2))) 
 (check-equal? (interp sub tester) 1)
-
 (define mult (BinopC '* (NumC 3) (NumC 2))) 
 (check-equal? (interp mult tester) 6)
-
 (define div (BinopC '/ (NumC 6) (NumC 2))) 
 (check-equal? (interp div tester) 3)
-
 (define leq_y (Ifleq0C (NumC -4) (NumC 6) (NumC 2))) 
 (check-equal? (interp leq_y tester) 6)
-
 (define leq_n (Ifleq0C (NumC 4) (NumC 6) (NumC 2))) 
 (check-equal? (interp leq_n tester) 2)
-
 (check-exn #rx"OAZO3 shouldn't get here" (lambda () (interp (IdC 'a) tester)))
   
-
-
 ;subst
 (check-equal? (subst (NumC 0) 'x (BinopC '+ (IdC 'x) (IdC 'y))) (BinopC '+ (NumC 0) (IdC 'y)))
 
@@ -240,4 +236,5 @@
 (check-equal? (is-allowed? 'ifleq0?) #f)
 (check-equal? (is-allowed? 'func) #f)
 (check-equal? (is-allowed? 'sym) #t)
-
+(check-exn #rx"key" (lambda () (parse-fundef '(func (+ x) : 13))))
+(check-exn #rx"syntax" (lambda () (parse '(+ b))))
